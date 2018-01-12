f3646000bf80ae7ae178e8dcbd4489f89a7045ff
caogy <caogy@luluyou.com>
2018-01-12 11:33:28 +0800

    测试测试

---
+++ FirstDemo/first.js
@@ -0,0 +1,15 @@
+var mainState = {
+    preload: function () {
+        game.load.image('logo', 'assets/a.png');
+    },
+    create: function () {
+        this.sprite = game.add.sprite(200, 150, 'logo');
+    },
+    update: function () {
+        this.sprite.angle += 1;
+    }
+};
+var game = new Phaser.Game(400, 300, Phaser.AUTO, 'gameDiv');
+game.state.add('main', mainState);
+game.state.start('main');
+//# sourceMappingURL=first.js.map
